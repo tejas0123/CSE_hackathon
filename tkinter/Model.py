5e37d1d111d80de75f17f0d7b9132ff66b89de11
tejas0123 <tejasrc27@gmail.com>
2022-11-25 15:00:30 +0530

    third commit

--- tkinter/Model.py
+++ tkinter/Model.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 from sklearn.metrics import confusion_matrix
 import joblib
+import pefile 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 # Reading Data set from csv file
 
@@ -48,12 +51,12 @@ for i in range(55):
 not_selected.append(54)
 new_df = df.drop(df.columns[not_selected],axis=1)
 
-sb.heatmap(corr_mat,
-vmin=-1, vmax=1, center=0,
-cmap=sb.diverging_palette(9, 189, n=100),
-square=True)
-plt.figure(figsize=(40,40)) 
-plt.show()
+# sb.heatmap(corr_mat,
+# vmin=-1, vmax=1, center=0,
+# cmap=sb.diverging_palette(9, 189, n=100),
+# square=True)
+# plt.figure(figsize=(40,40)) 
+# plt.show()
 
 print(new_df)
 
@@ -64,8 +67,7 @@ for i in range(22):
 
 # Spliting data set into training and testing data.
 # Training Model using training data
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+
 labels = Data['legitimate'].values
 
 legit_train,legit_test,mal_train,mal_test =train_test_split(new_df,labels,test_size=0.2)
@@ -78,7 +80,7 @@ print("The accuracy of the model is ",classifier.score(legit_test,mal_test)*100)
 # Confusion matrix 
 result = classifier.predict(legit_test)
 conf_mat = confusion_matrix(mal_test,result)
-print(conf_mat)
+# print(conf_mat)
 
 
 # Precision
@@ -94,25 +96,25 @@ F1_score = (2*precision*recall)/(precision+recall)
 print("F1-score :",F1_score)
 
 # Save model 
-joblib.dump(classifier,'Random_Forest_Model.sav')
+joblib.dump(classifier,'model.sav')
 
 
 # Test for individual sample 
-avg_values = new_df.mean()
-avg_values = pd.DataFrame(avg_values)
+# avg_values = new_df.mean()
+# avg_values = pd.DataFrame(avg_values)
 
-import pefile 
-pe = pefile.PE(r'C:\Windows\System32\takeown.exe');
-avg_SectionsMinRawsize = avg_values.iloc[12].values[0]
-avg_SectionsMinVirtualsize = avg_values.iloc[13].values[0]
-avg_ImportsNbDLL = avg_values.iloc[14].values[0]
-avg_ImportsNb = avg_values.iloc[15].values[0]
-avg_ImportsNbOrdinal = avg_values.iloc[16].values[0]
-avg_ExportNb = avg_values.iloc[17].values[0]
-avg_ResourcesNb = avg_values.iloc[18].values[0]
-avg_ResourcesMinEntropy = avg_values.iloc[19].values[0]
-avg_VersionInformationSize = avg_values.iloc[19].values[0]
-sample = [pe.FILE_HEADER.Machine,pe.FILE_HEADER.SizeOfOptionalHeader,pe.FILE_HEADER.Characteristics,pe.OPTIONAL_HEADER.MajorLinkerVersion,pe.OPTIONAL_HEADER.SizeOfCode,pe.OPTIONAL_HEADER.ImageBase,pe.OPTIONAL_HEADER.FileAlignment,pe.OPTIONAL_HEADER.MajorImageVersion,pe.OPTIONAL_HEADER.MinorImageVersion,pe.OPTIONAL_HEADER.MajorSubsystemVersion,pe.OPTIONAL_HEADER.SizeOfHeaders,pe.OPTIONAL_HEADER.Subsystem,avg_SectionsMinRawsize,avg_SectionsMinVirtualsize,avg_ImportsNbDLL,avg_ImportsNb,avg_ImportsNbOrdinal,avg_ExportNb,avg_ResourcesNb,avg_ResourcesMinEntropy,avg_VersionInformationSize]
-sample = pd.DataFrame([sample],columns=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
-print(classifier.predict(sample.values))
+
+# pe = pefile.PE(r'C:\Windows\System32\takeown.exe');
+# avg_SectionsMinRawsize = avg_values.iloc[12].values[0]
+# avg_SectionsMinVirtualsize = avg_values.iloc[13].values[0]
+# avg_ImportsNbDLL = avg_values.iloc[14].values[0]
+# avg_ImportsNb = avg_values.iloc[15].values[0]
+# avg_ImportsNbOrdinal = avg_values.iloc[16].values[0]
+# avg_ExportNb = avg_values.iloc[17].values[0]
+# avg_ResourcesNb = avg_values.iloc[18].values[0]
+# avg_ResourcesMinEntropy = avg_values.iloc[19].values[0]
+# avg_VersionInformationSize = avg_values.iloc[19].values[0]
+# sample = [pe.FILE_HEADER.Machine,pe.FILE_HEADER.SizeOfOptionalHeader,pe.FILE_HEADER.Characteristics,pe.OPTIONAL_HEADER.MajorLinkerVersion,pe.OPTIONAL_HEADER.SizeOfCode,pe.OPTIONAL_HEADER.ImageBase,pe.OPTIONAL_HEADER.FileAlignment,pe.OPTIONAL_HEADER.MajorImageVersion,pe.OPTIONAL_HEADER.MinorImageVersion,pe.OPTIONAL_HEADER.MajorSubsystemVersion,pe.OPTIONAL_HEADER.SizeOfHeaders,pe.OPTIONAL_HEADER.Subsystem,avg_SectionsMinRawsize,avg_SectionsMinVirtualsize,avg_ImportsNbDLL,avg_ImportsNb,avg_ImportsNbOrdinal,avg_ExportNb,avg_ResourcesNb,avg_ResourcesMinEntropy,avg_VersionInformationSize]
+# sample = pd.DataFrame([sample],columns=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+# print(classifier.predict(sample.values))
 
